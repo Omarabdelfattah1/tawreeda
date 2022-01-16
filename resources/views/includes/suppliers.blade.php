18a7966fc9e768cf4177881073fe035ac7938b76
Omar Abdelfattah <41128946+Omarabdelfattah1@users.noreply.github.com>
2022-01-16 15:49:29 +0200

    sagsa

--- resources/views/includes/suppliers.blade.php
+++ resources/views/includes/suppliers.blade.php
@@ -1,13 +1,13 @@
 @foreach($suppliers as $supplier)
-        
+
 <div class="col-lg-4 col-md-6 col-sm-8 text-center mb-6">
   <div class="card shadow-lg px-2 pb-3 mx-auto" style="position: relative;margin-top: 20%;">
     <a href="{{route('supplier',$supplier)}}">
-    <img src="{{asset($supplier->company_logo)}}" class="mx-auto pt-6" alt="" style="width:35%;position: absolute; top:-50%;left:32.5%;z-index: 11;padding-top: 50px;">
+    <img src="{{asset($supplier->company_logo)}}" class="mx-auto pt-6" alt="" style="width:35%;height:70%;position: absolute; top:-50%;left:32.5%;z-index: 11;padding-top: 50px;">
     </a>
     <h6 class="lead-3 font-weight-bold text-center pt-5" style="margin-top: 15%;padding-top: 2%;"> {{$supplier->company_name}}</h6>
     <p class="fs-5 mt-3 text-center">
-      <?php 
+      <?php
         echo str_repeat('<span class="fa fa-star" style="color: #f8c848;"></span>',(int)$supplier->stars()).
         str_repeat('<span class="fa fa-star"></span>',5-(int)$supplier->stars());
       ?>  | {{$supplier->reviews->count()}} تقييم
@@ -19,7 +19,7 @@
       <div class="btn btn-xs mx-auto" style="color:white;background-color: #44485C;">{{$supplier->state}}</div>
       @if($supplier->tagproducts()->first())
       <div class="btn btn-xs btn-secondary text-default mx-auto" >{{$supplier->tagproducts()->first()->name}}</div>
-      @endif 
+      @endif
     </div>
   </div>
 </div>
